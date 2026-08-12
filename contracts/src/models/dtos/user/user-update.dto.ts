b37425f6131500ec610e7e8d32e1238078e3f97b
parsanojavan <parsa.nojavan85@gmail.com>
2026-08-12 13:01:05 +0330

    feat(user): add user update dto

---
+++ contracts/src/models/dtos/user/user-update.dto.ts
@@ -0,0 +1,9 @@
+export interface UpdateUserDto {
+    firstName?: string;
+    lastName?: string;
+    username?: string;
+    bio?: string;
+    avatarUrl?: string;
+    phoneNumber?: string;
+    lang?: string;
+}
